d937f27a1d521fe12eb64d14ad7bd10da447d63d
kadir <kadiryolalan@gmail.com>
2023-07-07 15:48:27 +0300

    (python) Product model created

--- uav_rental/app/models.py
+++ uav_rental/app/models.py
@@ -1,1 +1,21 @@
 from django.db import models
+
+STATUS = (
+    (0,"avtive"),
+    (1,"passive")
+)
+# Create your models here.
+class Products(models.Model):
+    status = models.IntegerField(choices=STATUS, default=0)
+    name = models.CharField(max_length=200)
+    model = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    weight = models.FloatField()
+    updated_at = models.DateTimeField(auto_now= True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.name
