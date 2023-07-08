e4d0ec54b0e77095b7a97b5ff37c5616465c87ec
kadir <kadiryolalan@gmail.com>
2023-07-08 19:59:04 +0300

    (python) Rental CRUD view created

--- uav_rental/api/serializers.py
+++ uav_rental/api/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
-from app.models import Product, PRODUCT_STATUS, Category, CATEGORY_STATUS
+from app.models import Product, PRODUCT_STATUS, Category, CATEGORY_STATUS, Rental, RENTAL_STATUS
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'first_name', 'last_name')
 
 class CategorySerializer(serializers.ModelSerializer):
     name = serializers.CharField(required=True, max_length=200, min_length=3)
@@ -17,8 +23,21 @@ class ProductSerializer(serializers.ModelSerializer):
     brand = serializers.CharField(required=True, max_length=100, min_length=1)
     stocks = serializers.IntegerField(required=True, min_value=0)
     category = CategorySerializer(required=False, many=False)
-    category_id = serializers.PrimaryKeyRelatedField(source='category', queryset=Category.objects.all())
+    category_id = serializers.PrimaryKeyRelatedField(source='category', queryset=Category.objects.all(), allow_null=True)
 
     class Meta:
         model = Product
+        fields = ('__all__')
+
+class RentalSerializer(serializers.ModelSerializer):
+    status = serializers.ChoiceField(required=True, choices=RENTAL_STATUS)
+    product = ProductSerializer(required=False, many=False)
+    product_id = serializers.PrimaryKeyRelatedField(source='product', queryset=Product.objects.all(), allow_null=False)
+    user = UserSerializer(required=False, many=False)
+    user_id = serializers.PrimaryKeyRelatedField(source='user', queryset=User.objects.all(), allow_null=False)
+    started_at = serializers.DateTimeField(required=True)
+    expired_at = serializers.DateTimeField(required=True)
+
+    class Meta:
+        model = Rental
         fields = ('__all__')
--- uav_rental/api/views.py
+++ uav_rental/api/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import get_object_or_404
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.permissions import SAFE_METHODS, AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly, BasePermission, IsAdminUser, DjangoModelPermissions
-from app.models import Product, Category
-from .serializers import ProductSerializer, CategorySerializer
+from app.models import Product, Category, Rental
+from .serializers import ProductSerializer, CategorySerializer, RentalSerializer
 
 # API Views
 
@@ -59,6 +59,7 @@ class ProductRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
         else:
             return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
+
 class CategoryListCreate(generics.ListCreateAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = CategorySerializer
@@ -88,6 +89,48 @@ class CategoryRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
         serializer = self.get_serializer(
             instance, data=request.data, partial=True)
         if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+        else:
+            return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RentalListCreate(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = RentalSerializer
+
+    def get_queryset(self):
+        return Rental.objects.filter()
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data, partial=True)
+        if serializer.is_valid():
+            # ilgili product al status ve stocks kontrol et
+            # ilgili ürün için ilgili tarihlerde rental içerisinde arama yap
+            # eğer sayı stocks sayısını geçiyorsa oluşturamaz
+            # eğer sayı stocks sayısı altındaysa oluşturabilir
+            # bu sayede aynı tarihlerde olmayan ürünün kiralaması yapılamaz
+            # tüm bunların içerisinde her sorgulamada status kotnrolleri sağlanmalı
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+        else:
+            return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RentalRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = RentalSerializer
+
+    def get_object(self, queryset=None, **kwargs):
+        item = self.kwargs.get('pk')
+        return get_object_or_404(Rental, id=item)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(
+            instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            # post isteğiyle aynı kontroller olmalı fakat count alınırken kendisi dahil olmamalı bu detaya dikkat edilmeli
             serializer.save()
             return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
         else:
