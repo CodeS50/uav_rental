81d2c1b32dffbd6661c7ae2556d948c137a7101f
kadir <kadiryolalan@gmail.com>
2023-07-08 19:58:25 +0300

    (python) Rental model created

--- uav_rental/app/models.py
+++ uav_rental/app/models.py
@@ -1,13 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 PRODUCT_STATUS = (
-    (0, "Active"),
-    (1, "Passive")
+    (0, "Passive"),
+    (1, "Active")
 )
 
 CATEGORY_STATUS = (
-    (0, "Active"),
-    (1, "Passive")
+    (0, "Passive"),
+    (1, "Active")
+)
+
+RENTAL_STATUS = (
+    (0, "Pending"),
+    (1, "Active"),
+    (2, "Cancel")
 )
 # Create your models here.
 
@@ -35,10 +42,24 @@ class Product(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(
-        Category, on_delete=models.PROTECT, default=1)  # many-to-one
+        Category, on_delete=models.DO_NOTHING, blank=True, null=True)  # many-to-one
 
     class Meta:
         ordering = ['-created_at']
 
     def __str__(self):
         return self.name
+
+
+class Rental(models.Model):
+    status = models.IntegerField(choices=RENTAL_STATUS, default=0)
+    product = models.ForeignKey(
+        Product, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    started_at = models.DateTimeField(auto_now=False)
+    expired_at = models.DateTimeField(auto_now=False)
+
+    class Meta:
+        ordering = ['-created_at']
